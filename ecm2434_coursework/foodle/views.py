2b7040ec7558192c674fe14546b99fcf19bc9c15
JElder03 <jamiewpelder@gmail.com>
2024-02-27 02:23:36 +0000

    minor bug fixes

--- ecm2434_coursework/foodle/views.py
+++ ecm2434_coursework/foodle/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from users.models import UserProfile
 from .models import MealEvents
+from recipes.models import Recipes
 from .forms import CreateMealEvent
 from django.contrib.auth.decorators import login_required
 
 
+#TODO divert to join group page
 def welcome(request):
     return render(request, "foodle/welcome.html")
 
@@ -23,7 +25,7 @@ def play(request):
 @login_required
 def events(request):
     user_group = request.user.groups.first()
-    cooking_events = MealEvents.objects.filter(group=user_group)
+    cooking_events = MealEvents.objects.filter(group=user_group).order_by('-date_time')
     context = {"cooking_events": cooking_events}
     return render(request, "foodle/events.html", context)
 
@@ -37,7 +39,7 @@ def create_event(request):
                 user=request.user,
                 group=request.user.groups.first(),
                 date_time=form.cleaned_data.get("date_time"),
-                recipe=form.cleaned_data.get("recipe"),
+                recipe= Recipes.objects.get(recipe_id=form.cleaned_data.get("recipe")),
             )
             new_event.save()
             return redirect('foodle:events')
