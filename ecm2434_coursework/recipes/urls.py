4033471eabc299682943e6ac752cfa7ef50d2dae
JElder03 <jamiewpelder@gmail.com>
2024-02-28 22:51:55 +0000

    Minor HTML fixes (again)

--- ecm2434_coursework/recipes/urls.py
+++ ecm2434_coursework/recipes/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "recipes"
 urlpatterns = [
     path("create", views.create_recipe, name="create_recipe"),
-    path("/<int:recipe_id>", views.view_recipe, name="view_recipe"),
+    path("<int:recipe_id>", views.view_recipe, name="view_recipe"),
     path("", views.view_all_recipes, name="view_all_recipes"),
 ]
