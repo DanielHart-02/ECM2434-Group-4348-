d0cea8f573703d9e5e3973288913dd71f0576b72
Victor-M-Smith <111735724+Victor-M-Smith@users.noreply.github.com>
2024-02-28 16:40:20 +0000

    Added qrcode generator

---
+++ ecm2434_coursework/foodle/qr.py
@@ -0,0 +1,9 @@
+import qrcode
+from PIL import Image, ImageDraw
+import time
+
+
+def genQrCode(group_name):
+    qr_content = "http://127.0.0.1:8000/join_group/" + group_name
+    myqr = qrcode.make(qr_content)
+    return myqr
