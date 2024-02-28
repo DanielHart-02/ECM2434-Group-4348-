8dffebc4684f84b71cbe20a2d4ea90405fa159a6
Victor-M-Smith <111735724+Victor-M-Smith@users.noreply.github.com>
2024-02-28 16:38:32 +0000

    Update urls.py for recipe and group functionality

--- ecm2434_coursework/foodle/urls.py
+++ ecm2434_coursework/foodle/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path("group-cooks/", views.events, name="events"),
     path("group-cooks/create", views.create_event, name="create_event"),
     path("leaderboard/", views.leaderboard, name="leaderboard"),
+    path("createGroup", views.createGroup, name="createGroup"),
+    path("generateQrCode", views.generateQrCode, name="generateQrCode"),
+    path("recipe/<int:recipe_id>/", views.recipe_details, name="recipe_details"),
+    path("all_recipies", views.all_recipies, name="all_recipies"),
     path("no_group/", views.no_group, name="no_group"),
 ]
