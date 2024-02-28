45e1633f7049df666a4e2ee24a35b3568fe5ffd2
NotTodaySatan0 <dlxv82@durham.ac.uk>
2024-02-28 19:07:38 +0000

    Restructure and adjust to new db name scheme

--- ecm2434_coursework/recipes/views.py
+++ ecm2434_coursework/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.views.generic import CreateView
@@ -7,11 +7,16 @@ from .forms import RecipeForm
 
 
 @login_required
-def view_all_recipes(request):
-    return HttpResponse("To be built")
+def all_recipies(request):
+    recipes = Recipe.objects.all()
+    return render(request, "recipes/all_recipies.html", {"recipes": recipes})
 
-def view_recipe(request, recipe_id):
-    return HttpResponse("Viewing recipe %d" % recipe_id)
+
+@login_required
+def recipe_details(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, "recipes/recipe_details.html", {"recipe": recipe})
+
 
 def create_recipe(request):
     ingredient_ratings = IngredientRating.objects.all()
@@ -20,38 +25,43 @@ def create_recipe(request):
     if request.method == "POST":
         if form.is_valid():
             ingredients = form.cleaned_data.get("ingredients")
-            quantities = [int(quantity) for quantity in form.cleaned_data.get("quantities")]
+            quantities = [
+                int(quantity) for quantity in form.cleaned_data.get("quantities")
+            ]
             serves = form.cleaned_data.get("serves_num")
             co2_score = get_score(ingredients, quantities, serves)
-            
-            formatted_ingredients = ''
+
+            formatted_ingredients = ""
 
             for i in range(len(ingredients)):
-                formatted_ingredients += (ingredients[i] + ' - ' + str(quantities[i]) + 'g')
+                formatted_ingredients += (
+                    ingredients[i] + " - " + str(quantities[i]) + "g"
+                )
                 if i < len(ingredients) - 1:
-                    formatted_ingredients += ', '
+                    formatted_ingredients += ", "
 
             new_recipe = Recipe(
                 user=request.user,
-                recipe_title = form.cleaned_data.get("recipe_title"),
-                recipe_ingredients = formatted_ingredients,
-                preparation = form.cleaned_data.get("preparation"),
-                prep_time = form.cleaned_data.get("prep_time"),
-                serves_num = serves,
-                co2_per_portion = co2_score
+                recipe_title=form.cleaned_data.get("recipe_title"),
+                recipe_ingredients=formatted_ingredients,
+                preparation=form.cleaned_data.get("preparation"),
+                prep_time=form.cleaned_data.get("prep_time"),
+                serves_num=serves,
+                co2_per_portion=co2_score,
             )
             new_recipe.save()
-            return redirect('recipes:view_all_recipes')
+            return redirect("recipes:view_all_recipes")
         return render(request, "recipes/create_recipe.html", context)
     else:
         return render(request, "recipes/create_recipe.html", context)
-    
+
+
 def get_score(ingredients, quantities, serves):
     score = 0
     for i in range(len(ingredients)):
         try:
-            ingredient_rating = IngredientRating.objects.get(ingredient = ingredients[i])
-            score += ingredient_rating.rating/1000 * quantities[i]/serves
+            ingredient_rating = IngredientRating.objects.get(ingredient=ingredients[i])
+            score += ingredient_rating.rating / 1000 * quantities[i] / serves
         except IngredientRating.DoesNotExist:
             continue
     return score
